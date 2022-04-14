ac776473ed1869b451038eb9fa4655c6cdf2613c
Selin Termin <80844457+MelTermin@users.noreply.github.com>
2022-04-14 19:25:17 +0300

    changes on edit

--- client/src/components/Detail.js
+++ client/src/components/Detail.js
@@ -1,12 +1,16 @@
-import React,{useEffect,useState} from 'react'
+import React,{useEffect,useState,useContext} from 'react'
 import {useNavigate,useParams } from "react-router-dom";
 import axios from "axios";
+import FeedBackContext from "../context/contextApi"
+
 
 function Detail() {
+ const {setAllFeedback,allFeedBack}=useContext( FeedBackContext)
   let params = useParams(); 
   let navigate = useNavigate();
 
   const [comment,setComment]=useState("")
+  
 
 
 
@@ -23,14 +27,16 @@ function Detail() {
 
   const handleChange = async (e)=> {
     e.preventDefault();
-    await axios.put(`https://6239be97bbe20c3f66c93c18.mockapi.io/api/v1/feedback/${params.id}`,{
+     await axios.put(`https://6239be97bbe20c3f66c93c18.mockapi.io/api/v1/feedback/${params.id}`,{
       comment
+    }).then((data)=> {
+      setAllFeedback([...allFeedBack, data.data])
     })
-    navigate("/")
+      navigate("/")
   }
   return (
     <div>
-      <form onSubmit={(e)=>handleChange(e)}>
+      <form onSubmit={handleChange}>
 
       <input type="text" value={comment} onChange={(e)=>setComment(e.target.value)} ></input>
 
